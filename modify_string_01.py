6914c24d2b418afecac6a76c29ddb8c24fe31dad
Shridhar Shanbhag <shridhar107@gmail.com>
2020-03-10 00:34:36 +0530

    Update modify_string_01.py

--- modify_string_01.py
+++ modify_string_01.py
@@ -1,46 +1,34 @@
 # Python CHALLENGE
 '''
-Modify the given string
-
-Examples:
-    Example-1 
-        Input:
-            aaa
-        Output:
-            a3
-    Example-2:
-        Input:
-            aaaabbbbaaacccccc
-        Output:
-            a4b4a3c6
-
-Modify funnction 'get_result_str' such that it provides output string for given input string
+Modify function 'get_result_str' such that it provides output string for given input string
+Example-1 
+    Input:  'aaa'
+    Output: 'a3'
+Example-2:
+    Input:  'aaaabbbbaaacccccc'
+    Output: 'a4b4a3c6'
 '''
 
 def get_result_str(input_string):
     # print(input_string)
-    output_string = ''
+    prev_char = output_string = ''
     count_char = 1
-    prev_char = ''
-    for item in list(input_string):
-        if prev_char != item:
-            if len(output_string) != 0:
-                output_string += str(count_char)
-            output_string += item
+    for char_ in list(input_string):
+        if prev_char != char_:
+            if len(output_string) != 0: output_string += str(count_char)
+            output_string += char_
             count_char = 1
         else:
             count_char += 1
-        prev_char = item
+        prev_char = char_
     output_string += str(count_char)
     # print(output_string)
     return output_string
 
 def run_test_func():
-    a, b = 'SUCCESS', 'FAIL'
-    print({True:a , False:b } [get_result_str('aaa') == 'a3'])
-    print({True:a , False:b } [get_result_str('abababab') == 'a1b1a1b1a1b1a1b1'])
-    print({True:a , False:b } [get_result_str('aaaabbbbaaacccccc') == 'a4b4a3c6'])
-
+    print({True:'SUCCESS', False:'FAIL'} [get_result_str('aaa') == 'a3'])
+    print({True:'SUCCESS', False:'FAIL'} [get_result_str('abababab') == 'a1b1a1b1a1b1a1b1'])
+    print({True:'SUCCESS', False:'FAIL'} [get_result_str('aaaabbbbaaacccccc') == 'a4b4a3c6'])
 
 if __name__ == '__main__':
     run_test_func()
